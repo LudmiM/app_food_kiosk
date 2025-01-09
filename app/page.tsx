8749ba814d505c0517f7e81e05cdefba702cb08e
LudmiM <ludmi.m.1109@gmail.com>
2025-01-09 00:29:04 -0300

    Add strcuture initial for orders

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <p> Hollo word!!</p>
+    </div>
+  );
+}
---
+++ components/order/OrderSummary.tsx
@@ -0,0 +1,7 @@
+export default function OrderSummary() {
+  return (
+    <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
+        <h2 className="text-4xl text-center font-black">Mi pedido</h2>
+    </aside>
+  )
+}
