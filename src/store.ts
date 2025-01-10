e947e93d07880d94d1678e6608da2bc4dfbbd052
LudmiM <ludmi.m.1109@gmail.com>
2025-01-10 20:48:36 -0300

    Add function for add product in card

--- src/store.ts
+++ src/store.ts
@@ -2,14 +2,18 @@ import { create } from "zustand";
 import { OrderItem } from "./types";
 import { Product } from "@prisma/client";
 
-interface Store{
+interface Store {
     order: OrderItem[];
     addToCard: (product: Product) => void;
 }
 
-export const useStore = create<Store>(() => ({
-    order:[],
+export const useStore = create<Store>((set) => ({
+    order: [],
     addToCard: (product) => {
-        console.log(product)
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { categoryId, image, ...data } = product
+        set((state) => ({
+            order: [...state.order, { ...data, quantity: 1, subtotal: 1 * product.price }]
+        }));
     }
 }));
